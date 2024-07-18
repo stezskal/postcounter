1caba5cb3fc73f6cf42c9c596e8f6cd2a19f4f1b
Alex Stezskal <alex.stezskal@gmail.com>
2024-07-18 15:51:05 -0500

    working well, just added team name replacement

--- challenge.py
+++ challenge.py
@@ -3,6 +3,7 @@ import pymysql  # You can use the appropriate library for your database
 import os
 import matplotlib.pyplot as plt
 import get_attendance
+import argparse
 from datetime import datetime
 from tabulate import tabulate
 from datetime import datetime, timedelta
@@ -11,72 +12,69 @@ from datetime import datetime, timedelta
 from teams_input import Teams
 
 
-#force_str = input("Force new download from database(y/n)?")
-force_str='n'
-if('y' in force_str):
-    force_get_attendance = True
-else:
-    force_get_attendance = False
-## Read the data from pickle file if it exists, if not run and get data from SQL.
-file_path = 'attendance_data.pkl'
-if not os.path.exists(file_path) or force_get_attendance:
-    get_attendance.get_attendance_df_pickle(file_path)
-df = pd.read_pickle('attendance_data.pkl')
+def main(start_date: str, end_date: str, get_database: bool):
+
+    ## Read the data from pickle file if it exists, if not run and get data from SQL.
+    file_path = 'attendance_data.pkl'
+    if not os.path.exists(file_path) or get_database:
+        get_attendance.get_attendance_df_pickle(file_path)
+    df = pd.read_pickle('attendance_data.pkl')
 
 
-teams = Teams()
+    teams = Teams()
 
-print("Note must include leading zeros for the date entry below:")
-#start_date_str = input("Start date filter e.g. 2023-12-01:")
-#end_date_str = input("End date filter e.g. 2023-12-01:")
-#pax_str = input("PAX Name (hit return for all PAX):")
-
-#date=start_date_str
-date='2024-07-17'
-
-#while(end_date_str!=date):
-day_df = df[(df['Date'] == date)]
-
-## inplace changes the index of day_df and drop restarts index at 0 instead of the index from db which is like 37000
-#day_df.reset_index(inplace=True, drop=True)
-#
-#day_df.sort_values(by='Team', inplace=True)
-print(tabulate(day_df))
-
-noteam_list = []
-
-## add teams column to dataframe from today and sort by team
-teams.drop_downrange(day_df)
-teams.add_pax_homes_to_posts_df(day_df)
-teams.add_teams_to_posts_df(day_df)
-teams.add_ao_home_to_posts_df(day_df)
-
-#teams.evaluate_posts(day_df)
-#teams.check_q(day_df)
-
-print(tabulate(day_df))
-
-for index, row in day_df.iterrows():
-    pax = row['PAX']
-    q = row['Q']
-    ao = row['AO']
-    if(pax==q):
-        q_today=True
-        q_str="+Q Point!"
-    else:
-        q_today=True
-        q_str=""
-
-    team = row['Team']
-    if team=="NoTeam":
-        noteam_list.append(pax)
-
-        
-        print(f"{date}  Team {team}  {pax} scored post at {ao}! {q_str}")
+    print("Note must include leading zeros for the date entry below:")
+    #start_date_str = input("Start date filter e.g. 2023-12-01:")
+    #end_date_str = input("End date filter e.g. 2023-12-01:")
+    #pax_str = input("PAX Name (hit return for all PAX):")
 
 
-print(f"NoTeam PAX: {noteam_list}")
-print(f"Done")
+    #while(end_date_str!=date):
+    day_df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+
+    ## inplace changes the index of day_df and drop restarts index at 0 instead of the index from db which is like 37000
+    #day_df.reset_index(inplace=True, drop=True)
+    #
+    #day_df.sort_values(by='Team', inplace=True)
+    print(tabulate(day_df))
+
+    noteam_list = []
+
+    ## add teams column to dataframe from today and sort by team
+    teams.drop_downrange(day_df)
+    teams.add_pax_homes_to_posts_df(day_df)
+    teams.add_teams_to_posts_df(day_df)
+    teams.add_ao_home_to_posts_df(day_df)
+
+    teams.evaluate_posts(day_df)
+
+    team_results = teams.tally_team_points(day_df)
+    #teams.check_q(day_df)
+
+    print(tabulate(day_df, headers='keys', tablefmt='pretty'))
+
+    #for index, row in day_df.iterrows():
+    #    pax = row['PAX']
+    #    q = row['Q']
+    #    ao = row['AO']
+    #    if(pax==q):
+    #        q_today=True
+    #        q_str="+Q Point!"
+    #    else:
+    #        q_today=True
+    #        q_str=""
+    #
+    #    team = row['Team']
+    #    if team=="NoTeam":
+    #        noteam_list.append(pax)
+    #
+    #        
+    #        print(f"{date}  Team {team}  @{pax} scored post at {ao}! {q_str}")
+
+
+    print(tabulate(team_results, headers='keys', tablefmt='pretty'))
+    print(f"NoTeam PAX: {noteam_list}")
+    print(f"Done")
 
 
 
@@ -89,3 +87,12 @@ def increment_date_string(date_string):
     # Convert the datetime object back to a string
     new_date_string = new_date_object.strftime("%Y-%m-%d")
     return new_date_string
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some dates and a database flag.")
+    parser.add_argument('start_date', type=str, help='The start date')
+    parser.add_argument('end_date', type=str, help='The end date')
+    parser.add_argument('--get_database', action='store_true', help='Flag to get database')
+
+    args = parser.parse_args()
+    main(args.start_date, args.end_date, args.get_database)
---
+++ teams_input.py
@@ -0,0 +1,187 @@
+import sheets
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+from tabulate import tabulate
+
+import pandas as pd
+
+
+class Teams(object):
+
+    def __init__(self):
+        self._connect_google_sheets()
+        self._get_pax_teams_from_google_sheets()
+        self._get_aos_from_google_sheets()
+        self._get_names_from_google_sheets()
+
+    def _connect_google_sheets(self):
+        ## instantiate the sheets module (custom)
+        ## will use authentication to google sheets
+        ## make sure both google sheets have the service acct email shared
+        self.sheets_connection = sheets.sheets(name='f3_challenge_input')
+
+    def _get_pax_teams_from_google_sheets(self):
+        ws=self.sheets_connection.get_sheet('teams')
+        df = get_as_dataframe(ws, evaluate_formulas=True)
+
+        print(tabulate(df))
+
+        self.teams = df['Team'].unique()
+        print(self.teams)
+        team_dict={}
+        for team in self.teams:
+            paxs = df[df['Team']==team]['PAX'].unique()
+            team_dict[team]=paxs
+
+        print(tabulate(team_dict))
+
+        l=len(team_dict.keys())
+        if(l not in [8,9]):
+            raise Exception(f"expected to be 8 or 9 teams but there are {l} teams: {team_dict.keys()}")
+
+        self.team_dict = team_dict
+        self.pax_df = df
+
+
+
+    def _get_aos_from_google_sheets(self):
+        ws=self.sheets_connection.get_sheet('aos')
+        df = get_as_dataframe(ws, evaluate_formulas=True)
+        self.aos_df = df
+
+    def _get_names_from_google_sheets(self):
+        ws=self.sheets_connection.get_sheet('names')
+        df = get_as_dataframe(ws, evaluate_formulas=True)
+        self.team_names_df = df
+
+    
+    def get_team_for_pax(self, pax):
+        """ Use dataframe lookup for pax to see what team they are on and return it
+        """
+        df_match = self.pax_df[self.pax_df['PAX']==pax] ## look up that pax's information
+        df_match2 = df_match.reset_index()  ## reset index to 0 for easy access otherwise it'll have the index from the original df
+        team = df_match2.at[0,'Team']
+        return team
+
+    def get_home_for_pax(self, pax):
+        """ Use dataframe lookup for pax to see what team they are on and return it
+        """
+        df_match = self.pax_df[self.pax_df['PAX']==pax] ## look up that pax's information
+        df_match2 = df_match.reset_index()  ## reset index to 0 for easy access otherwise it'll have the index from the original df
+        home = df_match2.at[0,'Home']
+        return home
+        
+    def get_home_for_ao(self, ao):
+        """ Use dataframe lookup for ao to see what side it is on
+        """
+        df_match = self.aos_df[self.aos_df['AO']==ao] ## look up that pax's information
+        if df_match.shape[0]==0:
+            raise Exception(f"No entry for ao {ao} in google sheet aos, need to add with east or west designation.")
+        df_match2 = df_match.reset_index(drop=True)  ## reset index to 0 for easy access otherwise it'll have the index from the original df
+        home = df_match2.at[0,'Home']
+        return home
+
+    def drop_downrange(self, df):
+        ''' Drop any rows that have substring downrange in PAX name
+        '''
+
+        ## create a mask of all the rows that contain downrange for PAX
+        mask = df['PAX'].str.contains('downrange')
+        ## drop them in place for this df
+        df.drop(index=df[mask].index, inplace=True)
+        ## reset the indexes to account for the dropped rows
+        df.reset_index(inplace=True, drop=True)
+
+    def add_pax_homes_to_posts_df(self, df):
+        df['PAXHome']=''
+
+        ## reset index starting at 0, have to do this to use iloc below
+        df.reset_index(inplace=True, drop=True)
+
+        for index, row in df.iterrows():
+            pax = row['PAX']
+            ## Lookup team for that PAX
+            home = self.get_home_for_pax(pax) # gets E or W for that PAX
+            ## Assign E or W for that row in Home column
+            df.iloc[index, df.columns.get_loc('PAXHome')]=home
+
+        ## reset index starting at 0
+        df.reset_index(inplace=True, drop=True)
+
+        return df
+
+    def add_teams_to_posts_df(self, df):
+        df['Team']='' ## add a column for Team to be inserted
+
+        ## reset index starting at 0, have to do this to use iloc below
+        df.reset_index(inplace=True, drop=True)
+
+        for index, row in df.iterrows():
+            pax = row['PAX']
+            ## Lookup team for that PAX
+            team = self.get_team_for_pax(pax) # gets team name for that PAX
+            ## Assign the team for that row in Team column
+            df.iloc[index, df.columns.get_loc('Team')]=team
+
+        ## group/sort by teams
+        df.sort_values(by='Team', inplace=True)
+
+        ## reset index starting at 0
+        df.reset_index(inplace=True, drop=True)
+
+        return df
+
+    def add_ao_home_to_posts_df(self, df):
+        df['AOHome']=''
+        for index, row in df.iterrows():
+            post_ao=row['AO']
+            ao_home = self.get_home_for_ao(post_ao)
+            assert(ao_home in ['W','E', 'Away'])
+            df.iloc[index, df.columns.get_loc('AOHome')]=ao_home
+
+    def evaluate_posts(self, df):
+        df['Post Points']=0
+        df['Q Points']=0
+        df['Total Points']=0
+        for index, row in df.iterrows():
+            q_points=0
+            if row['PAX']==row['Q']:
+                q_points=1
+                df.iloc[index, df.columns.get_loc('Q Points')]=q_points
+
+            if row['PAXHome']==row['AOHome']:
+                post_points=1
+                df.iloc[index, df.columns.get_loc('Post Points')]=post_points
+            else:
+                post_points=2
+                df.iloc[index, df.columns.get_loc('Post Points')]=post_points
+
+            df.iloc[index, df.columns.get_loc('Total Points')]=post_points + q_points
+            
+
+    def tally_team_points(self, df):
+
+        d={}
+        for team in self.teams:
+            points = df.loc[df['Team'] == team, 'Total Points'].sum()
+            d[team]=points
+        
+            print(f"Team:{team}  points:{points}")
+
+        #self.team_pts_df = pd.DataFrame(d, index=[1], columns=['Points']).transpose().sort_values(by=[1], ascending=False)
+        pts_df = pd.DataFrame(d, index=[0]).transpose()
+        pts_df.rename(columns={0:'Points'}, inplace=True)
+        pts_df.sort_values(by='Points', ascending=False, inplace=True)
+
+        self._replace_team_names(pts_df)
+
+        self.pts_df = pts_df
+
+        return self.pts_df
+
+
+    def _replace_team_names(self,df):
+        for index, row in df.iterrows():
+            pax_team=row.name ## index is the team name
+            new_team = self.team_names_df[self.team_names_df['PAX']==pax_team].iloc[0,self.team_names_df.columns.get_loc('Name')]
+            df.rename(index={pax_team: new_team}, inplace=True)
+
